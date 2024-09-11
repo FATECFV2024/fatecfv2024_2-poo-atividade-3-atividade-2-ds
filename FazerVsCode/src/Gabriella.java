476c7ba0650327d8a3ff7b607555e81ef8305a15
Gabriella Sampaio Gomes de Castro <160081676+ggabl1@users.noreply.github.com>
2024-09-11 10:53:50 -0300

    Update and rename teste.java to Gabriella.java
    adicionando atividade 2

---
+++ FazerVsCode/src/Gabriella.java
@@ -0,0 +1,12 @@
+public class RA2920482411013 {
+
+    private String Completo = "Gabriella Sampaio Gomes de Castro";
+    private String RGM = "2920482411013";
+    private int Idade = 18;
+    private String gitHub = "ggabl1";
+    private String LinkedIn = "Gabriella Sampaio";
+    private String CursoeTurma = "análise e desenvolvimento de sistemas 2 semestre";
+
+    
+    
+}
