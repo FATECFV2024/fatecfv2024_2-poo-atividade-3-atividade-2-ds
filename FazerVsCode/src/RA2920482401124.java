22d95d59484bf1b7659ba6a2c36358736b139412
Paulo Henrique leal dos santos <101016549+PaulinhoLeal@users.noreply.github.com>
2024-09-11 12:19:10 -0300

    Adicionando atividade 2

---
+++ FazerVsCode/src/RA2920482401124.java
@@ -0,0 +1,11 @@
+public class RA2920482411024 {
+
+    //adicionando atributos 
+    private String nome = "Paulo Henrique";
+    private String ra = "2920482411024";
+    private int idade = 22;
+    private String gitHub= "PaulinhoLeal";
+    private String linkedin= "paulo-leal";
+    
+}
+
